2af9113645deae9f01d990a5b4976ccc83dd5a83
Qi Sun <141111060+q1sun@users.noreply.github.com>
2025-03-17 19:03:32 +0800

    Update run.sh

--- Sec411-Convection-Alternative-Augmentation/run.sh
+++ Sec411-Convection-Alternative-Augmentation/run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-mkdir -p 'user/'
-python3 -u main_Exp-Sec411-LinearConvection.py --weights 'Figures/Trained_Model/simulation_0'  --figures 'Figures/Python/simulation_0' > 'user/log_0.txt'
+python3 -u main_Exp-Sec411-LinearConvection.py --weights 'Figures/Trained_Model/simulation_0'  --figures 'Figures/Python/simulation_0'
